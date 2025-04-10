39467c2cb01e39d2027f14453a20ddacabb300d7
achaval-tomas <tomasachaval@mi.unc.edu.ar>
2025-04-10 12:54:37 +0000

    ejercicio 2b

---
+++ practico3/ej2.py
@@ -0,0 +1,20 @@
+from numpy.random import uniform
+
+print("*"*10 + "Ejercicio 2b" + "*"*10)
+
+def gameSim(Nsim):
+    win_count = 0
+
+    for _ in range(Nsim):
+        U = uniform()
+
+        value = uniform() + uniform()
+        if U >= 0.5:
+            value += uniform()
+        
+        win_count += (value >= 1)
+
+    return win_count/Nsim
+
+for N in [100, 1000, 10000, 100000, 1000000]:
+    print(f"Estimación de la probabilidad de ganar con N={N} -> {gameSim(N)}")
