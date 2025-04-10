6d654c842b3c22cfb4577c6edb4c75c88ab079d3
achaval-tomas <tomasachaval@mi.unc.edu.ar>
2025-04-10 20:42:05 +0000

    rerun ej4 with bigger n

--- practico3/ej4.py
+++ practico3/ej4.py
@@ -21,7 +21,7 @@ def waitLines(p1, e1, p2, e2, p3, e3, t, Nsim):
 
     return success/Nsim
 
-print(f"Valor obtenido con N=1000 -> {waitLines(0.4, 3, 0.32, 4, 0.28, 5, 4, 1000)}")
+print(f"Valor obtenido con N=1000000 -> {waitLines(0.4, 3, 0.32, 4, 0.28, 5, 4, 1000000)}")
 
 print()
 print("*"*10 + " Ejercicio 4b " + "*"*10)
@@ -50,7 +50,7 @@ def condWait(wait, e1, p1, e2, p2, e3, p3, Nsim):
 
     return lines[0]/waited_overtime, lines[1]/waited_overtime, lines[2]/waited_overtime
 
-c1, c2, c3 = condWait(4, 3, 0.4, 4, 0.32, 5, 0.28, 1000)
+c1, c2, c3 = condWait(4, 3, 0.4, 4, 0.32, 5, 0.28, 1000000)
 print(f"Probabilidad caja 1 dado que se esperaron 4 min -> {c1}")
 print(f"Probabilidad caja 2 dado que se esperaron 4 min -> {c2}")
 print(f"Probabilidad caja 3 dado que se esperaron 4 min -> {c3}")
