644fd9e8bcd9e231e847ce6de4c249b016260681
achaval-tomas <tomasachaval@mi.unc.edu.ar>
2025-04-11 23:27:09 +0000

    ej5 un poco mas clean

--- practico3/ej5.py
+++ practico3/ej5.py
@@ -6,19 +6,22 @@ def monteCarloIntegral(fun, a, b, iters):
     value = 0
 
     for _ in range(iters):
-        U = a + (b-a)*uniform()
+        U = a + (b - a)*uniform()
         value += fun(U)
     
     return value/iters
 
+def monteCarlo0inf(fun):
+    return lambda x : (1 / x**2) * fun(1/x - 1)
+
 print("*"*10 + " Ejercicio 5 a, b, c y d " + "*"*10)
 
-f_a = lambda x: (1-x**2)**1.5
-f_b = lambda x: x/(x**2-1)
+f_a = lambda x: (1 - x**2)**1.5
+f_b = lambda x: x / (x**2-1)
 
 # funciones del enunciado convertidas a h(x) en intervalo 0, 1
-f_c = lambda x: (1/x**2) * ((1/x - 1)*(1+(1/x - 1)**2)**(-2))
-f_d = lambda x: 2 * (1/x**2) * exp(-((1/x - 1)**2))
+f_c = monteCarlo0inf(lambda x: (x * (1 + x**2)**(-2)))
+f_d = monteCarlo0inf(lambda x: 2 * exp(-(x**2)))
 
 funciones = {
     "(1-x^2)^(3/2)" : (f_a, 0, 1),
@@ -29,8 +32,9 @@ funciones = {
 
 for f in funciones:
     print()
+    fun, a, b = funciones[f]
     for N in [1000, 10000, 100000, 1000000]:
-        print(f"iters: {N}, Integral entre {funciones[f][1]} y {funciones[f][2]} de {f} ~ {monteCarloIntegral(funciones[f][0], funciones[f][1], funciones[f][2], N)}")
+        print(f"iters: {N}, Integral entre {a} y {b} de {f} ~ {monteCarloIntegral(fun, a, b, N)}")
 
 
 print()
