c376b7ce11e40773930bfb74e79c7ea9f53f42e7
achaval-tomas <tomasachaval@mi.unc.edu.ar>
2025-06-03 13:56:47 +0000

    fix ej4

--- practico6/ej4.py
+++ practico6/ej4.py
@@ -14,7 +14,7 @@ z = norm.ppf(1 - 0.025)
 def sim(Niter):
     media = simN()
     SS, n = 0, 1
-    while (n < 100 or z * sqrt(SS/n) >= 0.025)*(not Niter) or (n <= Niter):
+    while (n < 100 or z * sqrt(SS/n) >= 0.0125)*(not Niter) or (n <= Niter):
         n += 1
         media_ant = media
         media = media_ant + (simN() - media_ant) / n
