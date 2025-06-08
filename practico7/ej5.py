3f7cba0efbc60abc0e4996f64cd46d93dea8cfd7
achaval-tomas <tomasachaval@mi.unc.edu.ar>
2025-06-08 02:12:18 +0000

    ej5 un poco mas generalizable

--- practico7/ej5.py
+++ practico7/ej5.py
@@ -2,9 +2,24 @@ from scipy.stats import chi2, binom
 
 data = [6, 7, 3, 4, 7, 3, 7, 2, 6, 3, 7, 8, 2, 1, 3, 5, 8, 7]
 n = len(data)
-k = 9
 
+k = 9
 pmf = lambda x,p : binom.pmf(x, 8, p)
+VAR_RANGE = range(k)
+
+def get_param(muestra):
+    return sum(muestra)/(8*n)
+
+def get_freqs(sample):
+    return [sample.count(v) for v in VAR_RANGE]
+
+def get_probs(p):
+    return [pmf(v, p) for v in VAR_RANGE]
+
+p_estim = get_param(data)
+def get_sample():
+    return list(binom.rvs(8, p_estim, size=n))
+
 
 group_indices = [
     [0, 1],
@@ -29,36 +44,29 @@ def estadistico(N, p, group=None):
         if n*esp>0
     )
 
-def get_freqs(sample, values):
-    return [sample.count(v) for v in values]
-
-def get_probs(p, values):
-    return [pmf(v, p) for v in values]
-
-p_estim = sum(data)/(8*n)
-freqs = get_freqs(data, range(k))
-probs = get_probs(p_estim, range(k))
+freqs = get_freqs(data)
+probs = get_probs(p_estim)
 
 g = lambda L : group(L, group_indices)
-
 t = estadistico(freqs, probs, g)
 
 def sim(Nsim):
     p_valor = 0
     for _ in range(Nsim):
-        muestra = list(binom.rvs(8, p_estim, size=n))
-        p_sim = sum(muestra)/(8*n)
+        muestra = get_sample()
+        p_sim = get_param(muestra)
         
-        freq_sim = get_freqs(muestra, range(k))
-        probs_sim = get_probs(p_sim, range(k))
+        freq_sim = get_freqs(muestra)
+        probs_sim = get_probs(p_sim)
 
         if estadistico(freq_sim, probs_sim, g) >= t:
             p_valor += 1
     
     return p_valor/Nsim
 
-grado = len(group_indices) - 1 - 1
 N = 10000
 print("***** Ejercicio 5 *****")
 print(f"p-valor ~ {sim(N)}")
-print(f"real ~ {1 - chi2.cdf(t, grado):5f}")
+
+grado_chi = len(group_indices) - 1 - 1
+print(f"real ~ {1 - chi2.cdf(t, grado_chi):5f}")
