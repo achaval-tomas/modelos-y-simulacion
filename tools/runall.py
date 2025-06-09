fb2c8612f88f8623eb7d41c2fe2a6fdc461139d2
achaval-tomas <tomasachaval@mi.unc.edu.ar>
2025-06-09 12:05:54 +0000

    ignorar errores en runall

--- tools/runall.py
+++ tools/runall.py
@@ -6,13 +6,12 @@ N = int(sys.argv[1])
 
 with open(f"../practico{N}/resultados.txt", "w") as f:
     for i in range(1, 20):
-        try:
-            output = subprocess.run(
-                ["python3", f"../practico{N}/ej{"0"*(i<10)}{i}.py"],
-                stdout=subprocess.PIPE,
-            ).stdout.decode('utf-8')
+        output = subprocess.run(
+            ["python3", f"../practico{N}/ej{"0"*(i<10)}{i}.py"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+        ).stdout.decode('utf-8')
 
-            f.write(output + "\n")
-        except FileNotFoundError:
-            pass
+        f.write(output + "\n")
+        
 
