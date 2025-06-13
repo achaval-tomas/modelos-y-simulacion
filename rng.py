2f83a0b4470f16486942080ff52f5c709a50ad9b
achaval-tomas <tomasachaval@mi.unc.edu.ar>
2025-06-13 18:59:13 +0000

    implement XORShift

--- rng.py
+++ rng.py
@@ -11,3 +11,17 @@ def LCG(seed):
     while True:
         value = (a * value) % m
         yield value
+
+
+def XORShift(seed):
+    """
+    Generador XORShift de 32 bits   
+    """
+    value = seed
+    mask_32b = 0xFFFFFFFF
+
+    while True:
+        value = (value ^ (value << 13)) & mask_32b
+        value = (value ^ (value >> 17)) & mask_32b
+        value = (value ^ (value << 5)) & mask_32b
+        yield value
