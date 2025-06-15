71a66ab844ba12cce1b0fd6878db324ffd007518
achaval-tomas <tomasachaval@mi.unc.edu.ar>
2025-06-15 23:35:06 +0000

    add instructions

--- integral.py
+++ integral.py
@@ -29,7 +29,7 @@ def calcular_varianza_muestral(xs):
     return sum((x - m) ** 2 for x in xs) / (len(xs) - 1)
 
 
-def test_generadores(seed, N=100):
+def test_generadores(seed, M=100):
     generators = [
         ("LCG", LCG(seed)),
         ("XORShift", XORShift(seed)),
@@ -39,26 +39,27 @@ def test_generadores(seed, N=100):
     print("Generador, d, N, ECM, Var, Tiempo")
     for name, gen in generators:
         for d in [2, 5, 10]:
-            for nsim in [10**4, 10**5, 10**6]:
+            for N in [10**4, 10**5, 10**6]:
                 start = time()
-                data = [integral_monte_carlo(gen, d, nsim) for _ in range(N)]
+                data = [integral_monte_carlo(gen, d, N) for _ in range(M)]
                 elapsed = time() - start
 
                 exacto = integral_exacta(d)
                 ecm = calcular_ecm(exacto, data)
                 var = calcular_varianza_muestral(data)
 
-                print(name, d, nsim, ecm, var, elapsed, sep=", ")
+                print(name, d, N, ecm, var, elapsed, sep=", ")
 
 
-# gen = Xoshiro(random.randint(0, 2**32 - 1))
-# d = 3
-# estimado = integral_monte_carlo(gen, d, nsim=100_000)
-# exacto = (0.5 * math.sqrt(math.pi) * math.erf(1)) ** d
+"""
+    Para obtener los resultados de un test, se debe seleccionar
+    una semilla s_0 para los generadores, un tamaño de muestra
+    M para las estimaciones de ECM y Var del estimador
+    y luego correr
 
-# print(f"d: {d}")
-# print(f"estimación: {estimado}")
-# print(f"exacto: {exacto}")
-# print(f"diferencia: {abs(estimado - exacto)}")
+    test_generadores(s_0, M)
 
-test_generadores(1239)
+    que mostrará en consola los resultados obtenidos.
+
+    El informe del trabajo fue realizado con s_0 = 1239 y M = 100.
+"""
